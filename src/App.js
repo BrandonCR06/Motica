938dfe96c7c7b2bc29f042017f1bc34e6fcaec25
BrandonCR06 <rjbrandon0@gmail.com>
2022-09-03 23:42:32 -0600

    Footer y diseño

--- src/App.js
+++ src/App.js
@@ -1,35 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="">
-
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Motica</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Preguntas frecuentes</a>
-              <a class="nav-link" href="#">¿Quienes somos?</a>
-              <a class="nav-link" href>Contactos</a>
-            </div>
-          </div>
-        </div>
+ 
+      <nav class="bg-success text-white navbar">
+      <a class="navbar-brand text-white" href="#">MOTICA</a>
+          <button class="btn btn-outline-light me-2 position-absolute top-10 end-0" type="button">INICIAR SESION</button>
       </nav>
 
-      <h2 className='text-center'>Bienvenido a motica, la mejor mota de costa rica</h2>
-      
-      <div class="text-center">
-        <img class="rounded" src="https://www.revistamoi.com/wp-content/uploads/2018/04/mota-1024x580.jpg" alt="..."></img>
+      <div class="position-absolute top-50 start-0 translate-middle-y">
+        <h1>Bienvenido a motica</h1>
+        <h3>Tienda en linea de productos derivados del cannabis</h3>
+        <a href="#" class="btn btn-default btn-lg">TIENDA</a>
       </div>
 
+
+      <section class="">
+        <footer class="bg-success text-white text-center fixed-bottom">
+          <div class="container p-4">
+            <div class="row">
+              <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Footer Content</h5>
+
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                  molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+                  aliquam voluptatem veniam, est atque cumque eum delectus sint!
+                </p>
+              </div>
+
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Empresa</h5>
+
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#!" class="text-white">Historia</a>
+                  </li>
+                  <li>
+                    <a href="#!" class="text-white">Descripcion del producto</a>
+                  </li>
+                  <li>
+                    <a href="#!" class="text-white">Organigrama de la empresa</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-0">De interes</h5>
+
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="#!" class="text-white">Ubicacion</a>
+                  </li>
+                  <li>
+                    <a href="#!" class="text-white">Preguntas frecuentes</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </footer>
+      </section>
+
+
+
+
+
       
+
+
+
     </div>
+
+
+    
+
+
+
   );
 }
 

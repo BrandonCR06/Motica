92956bb2ecb8ce4b6019f048c4914f35680c2ee9
MaickF <53355551+MaickF@users.noreply.github.com>
2022-09-15 21:35:06 -0600

    Ajustes de pantalla (Descripcion del producto)

--- src/Descripcion.js
+++ src/Descripcion.js
@@ -30,7 +30,10 @@ function Descripcion() {
 
      <div class= "text-success position-relative backgroundDescription">
         <br/>
+       <br/>
         <h1>Descripción del sitio</h1>
+          <br/>
+          <br/>
           <div class = "text-white position-relative parrafo">
           <p>Se conoce con el nombre de cannabis con sus variantes, sativa e índica, una planta, conocida en botánica como Cannabis sativa que ha sido utilizada desde la antigüedad por sus propiedades. La planta se ha usado y se usa actualmente con diferentes fines: recreativos, medicinales e industriales se puede utilizar para confeccionar ropa, cuerda, zapatos, papel y otros. <br/> Nuestra empresa se encarga de comerciar de forma legal el cannabis en todas sus formas aplicativas. Nuestro objetivo es dominar el mercado de dicho producto y polularizar su uso como cualquier otra droga recreativa anteriormente legalizada, con el fin de eliminar el tabú creado alrededor de este. Dicho objetivo también conlleva campañas de conscienciación sobre su uso.</p>
           </div>
@@ -38,7 +41,7 @@ function Descripcion() {
 
    
         <section class="">
-        <footer class="backgroundNavbar text-white text-center position-relative fixed-bottom">
+        <footer class="backgroundNavbar text-white text-center fixed-bottom">
           <div class="container p-0">
             <div class="row">
 
